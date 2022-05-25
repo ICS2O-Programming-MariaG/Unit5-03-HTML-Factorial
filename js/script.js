d986081914d3aa78948a2b33eb68874f829e1543
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-25 15:06:18 +0000

    Added form and div for results

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria G
+// Created on: May 25, 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function numSent() {
+  let userNum = parseInt(document.getElementById("number").value);
+}
